d31178d9128e4a1111db2ac887acc64754221d5f
criscasas <cristiancasas706@gmail.com>
2024-05-26 23:31:44 -0500

    get de la última Fase de un procesoReq

--- src/controllers/procesoRequerimiento.controllers.js
+++ src/controllers/procesoRequerimiento.controllers.js
@@ -1,5 +1,6 @@
 import oracleDB from "oracledb";
 
+// GET - Obtener todos los Procesos Requerimientos
 export const getProcesosRequerimientos = async (req, res) => {
     try {
         const dbConnection = await oracleDB.getConnection("myPool");
@@ -27,5 +28,42 @@ export const getProcesosRequerimientos = async (req, res) => {
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: 'Error interno del servidor' })
+    }
+}
+
+
+
+// GET - Obtener un ProcesoRequerimiento según el idreq y idproc
+
+export const getUltimaFase = async (req, res) => {
+    try {
+        const dbConnection = await oracleDB.getConnection("myPool");
+
+        const { consecReque, consecProceso } = req.params;
+
+        const ultimaFase = await dbConnection.execute(
+            `
+                SELECT *
+                FROM PROCESOREQUERIMIENTO
+                WHERE CONSECREQUE = :consecReque
+                AND CONSPROCESO = :consecProceso
+                AND IDFASE = (
+                    SELECT MAX(IDFASE)
+                    FROM PROCESOREQUERIMIENTO
+                    WHERE CONSECREQUE = :consecReque
+                    AND CONSPROCESO = :consecProceso
+                )
+            `,
+            [consecReque, consecProceso, consecReque, consecProceso]
+        );
+
+        if (ultimaFase.rows.length === 0) {
+            return res.status(200).json({ message: 'No se encontraron fases para ese CONSECREQUE y CONSPROCESO' });
+        }
+
+        return res.status(200).json(ultimaFase.rows[0]);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Error interno del servidor' });
     }
 }
--- src/routes/procesoRequerimiento.routes.js
+++ src/routes/procesoRequerimiento.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getProcesosRequerimientos } from "../controllers/procesoRequerimiento.controllers.js";
+import { getProcesosRequerimientos, getUltimaFase} from "../controllers/procesoRequerimiento.controllers.js";
 
 const router = Router();
 
 //Obtener todas las Disciplinas
 router.get("/procesosRequerimientos/:codEmpleado", getProcesosRequerimientos);
+
+//Obtener un ProcesoRequerimiento según el idreq y idproc
+router.get("/ultimaFase/:consecReque/:consecProceso", getUltimaFase);
+
 
 export default router;
