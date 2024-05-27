8b7f08ab44094ddb51a7f4d147dc0c3aa4d09296
Cristian Espitia <cristianespitia2506@gmail.com>
2024-05-27 04:20:37 -0500

    Format

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import cors from "cors";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
 
-import cargoRoutes from "./routes/cargo.routes.js"
-import requerimientoRoutes from "./routes/requerimiento.routes.js"
-import disciplinaRoutes from "./routes/disciplina.routes.js"
-import perfilRoutes from "./routes/perfil.routes.js"
-import procesoRequerimientoRoutes from "./routes/procesoRequerimiento.routes.js"
+import cargoRoutes from "./routes/cargo.routes.js";
+import requerimientoRoutes from "./routes/requerimiento.routes.js";
+import disciplinaRoutes from "./routes/disciplina.routes.js";
+import perfilRoutes from "./routes/perfil.routes.js";
+import procesoRequerimientoRoutes from "./routes/procesoRequerimiento.routes.js";
 
 const app = express();
 
@@ -24,14 +24,14 @@ app.use(perfilRoutes);
 app.use(procesoRequerimientoRoutes);
 
 const start = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT);
+  try {
+    await connectDB();
+    app.listen(PORT);
 
-        console.log("Servidor corriendo en el puerto:", PORT);
-    } catch (error) {
-        console.log(error);
-    }
-}
+    console.log("Servidor corriendo en el puerto:", PORT);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
